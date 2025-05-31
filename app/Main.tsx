928f8a47c3c69772fd9be862e5158287309ee8c5
SkyeClover <jacobwalker852@gmail.com>
2025-05-31 19:38:17 -0400

    Fix

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,7 +3,6 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const MAX_DISPLAY = 5
 
